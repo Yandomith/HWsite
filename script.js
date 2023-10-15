c4076a1d06208080cab62f7b7ca1b361fc66f012
Yandomith <yandomith@gmail.com>
2023-10-16 02:52:25 +0545

    optimized

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Function to generate the custom size image with customization
 function generateImage() {
     const text = document.getElementById('text-input').value;
-
-    const imageWidth = 2480; // Set to A4 width in millimeters
-    const imageHeight = 3508; // Set to A4 height in millimeters
-    const textOpacity = parseFloat(document.getElementById('text-opacity').value); // Get text opacity value
+    const imageWidth = 2480; // A4 width in millimeters
+    const imageHeight = 3508; // A4 height in millimeters
+    const textOpacity = parseFloat(document.getElementById('text-opacity').value);
     const topMargin = parseInt(document.getElementById('top-margin').value);
     const bottomMargin = parseInt(document.getElementById('bottom-margin').value);
     const leftMargin = parseInt(document.getElementById('left-margin').value);
@@ -13,7 +11,7 @@ function generateImage() {
     const textColor = document.getElementById('text-color').value;
     const lineSpacing = parseInt(document.getElementById('line-spacing').value);
     const backgroundSelect = document.getElementById('background-select');
-    const selectedBackground = backgroundSelect.value; // Get the selected background image
+    const selectedBackground = backgroundSelect.value;
 
     if (text.trim() !== '') {
         const canvas = document.createElement('canvas');
@@ -21,19 +19,16 @@ function generateImage() {
         canvas.width = imageWidth;
         canvas.height = imageHeight;
 
-        // Set background color to white
         context.fillStyle = '#FFFFFF';
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        // Draw selected background image if provided
         if (selectedBackground) {
             const backgroundImage = new Image();
-            backgroundImage.src = selectedBackground; // Use the selected background image URL
+            backgroundImage.src = selectedBackground;
             backgroundImage.onload = function () {
                 backgroundImage.width = canvas.width;
                 backgroundImage.height = canvas.height;
                 context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-
                 drawText();
             };
         } else {
@@ -41,22 +36,16 @@ function generateImage() {
         }
 
         function drawText() {
-            context.globalAlpha = textOpacity; // Set text opacity
-            context.fillStyle = textColor; // Text color with opacity
-            context.font = textSize + 'px Arial'; // Font size and style
-            context.textBaseline = 'top'; // Start text from the top
+            context.globalAlpha = textOpacity;
+            context.fillStyle = textColor;
+            context.font = textSize + 'px Arial';
+            context.textBaseline = 'top';
 
-            // Calculate available vertical space between top and bottom margins
             const availableHeight = canvas.height - topMargin - bottomMargin;
-
-            // Calculate available width between left and right margins
             const availableWidth = canvas.width - leftMargin - rightMargin;
 
-            // Split text into lines and handle wrapping
-            // Split text into lines and handle wrapping and line breaks
-           // Split text into lines and handle wrapping and line breaks
             const lines = [];
-            const paragraphs = text.split('\n'); // Split text into paragraphs using Enter key
+            const paragraphs = text.split('\n');
 
             paragraphs.forEach((paragraph) => {
                 const words = paragraph.split(' ');
@@ -77,38 +66,32 @@ function generateImage() {
                 lines.push(currentLine.trim());
             });
 
-            // Calculate vertical position to align text to the top
             let y = topMargin;
 
-            // Draw text line by line with line spacing and word wrapping
             lines.forEach((line) => {
-                // Draw the line and apply line spacing
                 context.fillText(line, leftMargin, y);
-
-                // Move to the next line
                 y += textSize + lineSpacing;
             });
 
-            // Convert canvas to image
-            const image = canvas.toDataURL('image/png');
+        
 
-            // Display the generated image
+            const image = canvas.toDataURL('image/png');
             const outputImage = document.getElementById('output-image');
             outputImage.src = image;
         }
+
+
+        
     } else {
         alert('Please enter text to generate an image.');
     }
 }
 
-// Add event listeners to all input fields for real-time updates
 const inputFields = document.querySelectorAll('.form-control');
 inputFields.forEach((inputField) => {
     inputField.addEventListener('input', generateImage);
 });
 
-
-// Add event listener to the "Download Image" button
 document.getElementById('download-button').addEventListener('click', function () {
     const image = document.getElementById('output-image').src;
     const link = document.createElement('a');
@@ -117,6 +100,4 @@ document.getElementById('download-button').addEventListener('click', function ()
     link.click();
 });
 
-
-// Call the initial image generation
 generateImage();
