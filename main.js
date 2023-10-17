f3da1b18d22a1ed7b4a25b659789161cff9e2573
Yandomith <yandomith@gmail.com>
2023-10-17 22:12:46 +0545

    cde

--- main.js
+++
@@ -1,5 +0,0 @@
-import { generateImage } from './imageGeneration';
-import { addEventListeners } from './eventListeners';
-
-generateImage(); // Call generateImage initially
-addEventListeners(); // Add event listeners
